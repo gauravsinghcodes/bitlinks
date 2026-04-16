bd76c643c78c4c8007136eb483e9f765dbf742fe
Gaurav Singh <gs7august@gmail.com>
2026-04-17 01:42:54 +0530

    Change

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,23 +1,26 @@
-
 import clientPromise from "@/lib/mongodb"
 
 export async function POST(request) {
+    try {
+        const body = await request.json() 
+        const client = await clientPromise;
+        const db = client.db("bitlinks")
+        const collection = db.collection("url")
 
-    const body = await request.json() 
-    const client = await clientPromise;
-    const db = client.db("bitlinks")
-    const collection = db.collection("url")
+        // Check if the short url exists
+        const doc = await collection.findOne({shorturl: body.shorturl})
+        if(doc){
+            return Response.json({success: false, error: true,  message: 'URL already exists!' })
+        }
 
-    // Check if the short url exists
-    const doc = await collection.findOne({shorturl: body.shorturl})
-    if(doc){
-        return Response.json({success: false, error: true,  message: 'URL already exists!' })
+        const result = await collection.insertOne({
+            url: body.url,
+            shorturl: body.shorturl
+        })
+
+        return Response.json({success: true, error: false,  message: 'URL Generated Successfully' })
+    } catch (error) {
+        console.error("Database Error:", error);
+        return Response.json({ success: false, error: true, message: 'Internal Server Error. Check your DB connection.' }, { status: 500 })
     }
-
-    const result = await collection.insertOne({
-        url: body.url,
-        shorturl: body.shorturl
-    })
-
-    return Response.json({success: true, error: false,  message: 'URL Generated Successfully' })
-  }
+}
